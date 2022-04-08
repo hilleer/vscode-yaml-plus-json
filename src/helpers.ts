3f7f64b1077b78535c850bcdad0a2ab4fb0c1fec
Daniel Hillmann <hiller@live.dk>
2022-04-08 19:54:18 +0200

    feature: add command to preview conversion (#48)
    * feature: add command to preview conversion
    
    * convert text
    
    * changes
    
    * readme
    
    * shorten
    
    * changelog file
    
    * force chance
    
    * revert
    
    * force changes
    
    * revert forced changes

--- src/helpers.ts
+++ src/helpers.ts
@@ -27,7 +27,9 @@ export function getYamlFromJson(json: string): string {
 	};
 
 	try {
-		return YAML.stringify(JSON.parse(json), options);
+		const jsonObject = JSON.parse(json);
+
+		return YAML.stringify(jsonObject, options);
 	} catch (error) {
 		console.error(error);
 		throw new Error('Failed to parse YAML. Please make sure it has a valid format and try again.');
@@ -37,6 +39,7 @@ export function getYamlFromJson(json: string): string {
 export function getJsonFromYaml(yaml: string): string {
 	try {
 		const json = YAML.parse(yaml, {});
+
 		return JSON.stringify(json, undefined, 2);
 	} catch (error) {
 		console.error(error);
