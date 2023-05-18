4c1d91bfdc40d2a8e745fa26ecb13d50865c78c7
Daniel Hillmann <hiller@live.dk>
2023-05-18 11:32:37 +0200

    build: replace vscode test package (#169)
    * build: replace vscode test package
    
    * update engines version
    
    * print test error
    
    * changes
    
    * update node versions tested on
    
    * set node engines
    
    * update package lock

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
 
 process.on('uncaughtException', () => {}); // ignore those, expected, should still be able to rely on tests
 
@@ -17,6 +17,7 @@ async function main() {
 		// Download VS Code, unzip it and run the integration test
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
+		console.error(err);
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -2,15 +2,15 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import { glob } from 'glob';
 
+// Create the mocha test
+const mocha = new Mocha({
+	ui: 'tdd',
+	color: true
+});
+
+const testsRoot = path.resolve(__dirname, '..');
+
 export function run(): Promise<void> {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
-
-	const testsRoot = path.resolve(__dirname, '..');
-
 	return new Promise(async (c, e) => {
 		const files = await glob('**/**.test.js', { cwd: testsRoot });
 		// Add files to the test suite
@@ -20,10 +20,10 @@ export function run(): Promise<void> {
 			// Run the mocha test
 			mocha.run(failures => {
 				if (failures > 0) {
-					e(new Error(`${failures} tests failed.`));
-				} else {
-					c();
+					return e(new Error(`${failures} tests failed.`));
 				}
+
+				c();
 			});
 		} catch (err) {
 			console.error(err);
